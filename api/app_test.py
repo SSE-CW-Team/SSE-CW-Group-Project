97ae9af41cacf04483462dc4f0b05a16b1c9b042
Julius Gasson <jg2023@ic.ac.uk>
2023-11-27 21:59:02 +0000

    Created basic webapp with welcome and query pages

--- api/app_test.py
+++ api/app_test.py
@@ -1,2 +1,16 @@
-def test_base():
-    assert (True)
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_welcome_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_submit_route(client):
+    response = client.get('/query')
+    assert response.status_code == 200

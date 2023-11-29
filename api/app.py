fb4c6434a035b3989ca356d4dfa556226ec79b2c
Patrick Borman <patrick.borman@imperial.ac.uk>
2023-11-29 16:02:04 +0000

    Build: changed front page and removed unnecessary files

--- api/app.py
+++ api/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def welcome():
-    return render_template("index.html")
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.form
+    distance = data.get('distance')
+    location = data.get('location')
+    genre = data.get('genre')
+    intensity = data.get('intensity')
 
+    # Here you would add the logic to generate the running route and playlist
+    # For now, let's just return the received data
+    return jsonify({
+        'distance': distance,
+        'location': location,
+        'genre': genre,
+        'intensity': intensity,
+        'route': 'Route generation logic goes here',
+        'playlist': 'Playlist generation logic goes here'
+    })
 
-@app.route("/query")
-def submit():
-    return render_template("query.html")
+if __name__ == '__main__':
+    app.run(debug=True)

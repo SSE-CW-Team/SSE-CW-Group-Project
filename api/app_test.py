21ab21c8472d4f9c8d70ff81b58a1774a32e12b3
Ankur Desai <avd123@ic.ac.uk>
2023-12-01 20:07:39 +0000

    Test: Added test to check generated playlist is of correct length given run length

--- api/app_test.py
+++ api/app_test.py
@@ -1,26 +1,27 @@
 from app import app
+from app import get_songs_from_database
 import pytest
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
 def test_welcome_route(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
 
 
 def test_login_redirect(client):
-    response = client.get('/generate')
+    response = client.get("/generate")
     assert response.status_code == 302  # Redirect
 
 
 def test_success_route(client):
-    response = client.get('/success')
+    response = client.get("/success")
     assert response.status_code == 200
 
 
@@ -29,3 +30,12 @@ def test_redirect_from_spotify(client):
     assert True  # I don't know how to do this yet.
     # Selenium: requires you to have access to where the browser is stored
     # Requests: I don't know how to post the login details
+
+
+def test_positive_mins_returns_data():
+    assert get_songs_from_database(20)[0] != []
+
+
+def test_playlist_length_exceeds_run_length():
+    mins = 30
+    assert get_songs_from_database(mins)[1] > mins

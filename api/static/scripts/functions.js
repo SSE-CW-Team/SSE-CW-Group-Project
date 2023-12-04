b30479008de90fc732013f0f6c3517e468635157
avdes96 <140278723+avdes96@users.noreply.github.com>
2023-12-04 17:15:04 +0000

    Feature: Updated genre input to account for duplicates and invalid selections

--- api/static/scripts/functions.js
+++ api/static/scripts/functions.js
@@ -16,7 +16,51 @@ function filterOptions() {
 
 // Function to add selected option to the list
 function addToSelectedList() {
-  var selectedItem = document.getElementById("genre").value;
+  var inputElement = document.getElementById("genre");
+
+  // Check if the input box is empty
+  if (inputElement.value.trim() === "") {
+    return; // Exit early if there is no text
+  }
+  var selectedItem = inputElement.value.trim().toLowerCase();
+  var dropdownOptions = document.getElementById("genreList").getElementsByTagName("option");
+
+  // Filter dropdown options based on user input
+  var filteredOptions = Array.from(dropdownOptions).filter(
+    (option) => option.value.toLowerCase().indexOf(selectedItem) !== -1
+  );
+
+  if (filteredOptions.length === 0) {
+    return; // Exit early if there are no filtered options
+  }
+
+  // Check if there are matching options in the filtered list
+  var matchingOption = filteredOptions.length > 0 ? filteredOptions[0] : null;
+
+  // If there is no match in the filtered list, use the top value from the original dropdown
+  if (!matchingOption && dropdownOptions.length > 0) {
+    matchingOption = dropdownOptions[0];
+  }
+
+  // Add the selected option to the list
+  addOptionToSelectedList(matchingOption.value);
+
+  // Clear the input field
+  inputElement.value = "";
+}
+
+// Event listener for keydown event on the input field
+document.getElementById("genre").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    // Prevent the default behavior of the Enter key
+    event.preventDefault();
+    addToSelectedList(); // Simulate a click on the most relevant value
+  }
+  console.log(selectedList)
+});
+
+// Function to add the selected option to the list
+function addOptionToSelectedList(selectedItem) {
   var selectedList = document.getElementById("selectedList");
   var listItem = document.createElement("li");
   listItem.textContent = selectedItem;
@@ -26,20 +70,80 @@ function addToSelectedList() {
   removeButton.textContent = "Remove";
   removeButton.classList.add("remove");
   removeButton.addEventListener("click", function () {
+    var removedGenre = listItem.textContent.trim();
     listItem.remove();
     updateHiddenInput(); // Update the hidden input when removing an item
+    addOptionBackToDropdown(removedGenre); // Add the removed option back to the dropdown
   });
 
   // Append the remove button to the list item
   listItem.appendChild(removeButton);
 
-  // Append the list item to the selected list
-  selectedList.appendChild(listItem);
+  // Find the correct position to insert the new item in alphabetical order
+  var existingItems = selectedList.getElementsByTagName("li");
+  var insertIndex = 0;
 
-  // Clear the input field
-  document.getElementById("genre").value = "";
+  for (var i = 0; i < existingItems.length; i++) {
+    var existingItemText = existingItems[i].textContent.trim();
+    if (selectedItem.toLowerCase() > existingItemText.toLowerCase()) {
+      insertIndex = i + 1;
+    } else {
+      break;
+    }
+  }
+
+  // Insert the new item at the correct position
+  if (insertIndex < existingItems.length) {
+    selectedList.insertBefore(listItem, existingItems[insertIndex]);
+  } else {
+    selectedList.appendChild(listItem);
+  }
+
+  // Remove the selected genre from the dropdown list
+  removeOptionFromDropdown(selectedItem);
 
   updateHiddenInput(); // Update the hidden input when adding an item
+
+  printSelectedListState();
+}
+
+// Function to print the current state of the selected list to the console
+function printSelectedListState() {
+  var selectedGenres = getSelectedGenres();
+  console.log("Selected List State:", selectedGenres);
+}
+
+
+// Function to remove the selected option from the dropdown list
+function removeOptionFromDropdown(optionText) {
+  var dropdownOptions = document.getElementById("genreList").getElementsByTagName("option");
+  for (var i = 0; i < dropdownOptions.length; i++) {
+    if (dropdownOptions[i].value.toLowerCase() === optionText.toLowerCase()) {
+      dropdownOptions[i].remove();
+      break;
+    }
+  }
+}
+
+// Function to add the removed option back to the dropdown list at the correct alphabetical spot
+function addOptionBackToDropdown(optionText) {
+  var dropdownOptions = document.getElementById("genreList").getElementsByTagName("option");
+  for (var i = 0; i < dropdownOptions.length; i++) {
+    if (optionText.toLowerCase() < dropdownOptions[i].value.toLowerCase()) {
+      var newOption = document.createElement("option");
+      optionText = optionText.endsWith("Remove") ? optionText.slice(0, -"Remove".length) : optionText;
+      newOption.value = optionText;
+      dropdownOptions[i].parentNode.insertBefore(newOption, dropdownOptions[i]);
+      break;
+    }
+  }
+  // If the option should be added at the end
+  if (i === dropdownOptions.length) {
+    var newOption = document.createElement("option");
+    optionText = optionText.endsWith("Remove") ? optionText.slice(0, -"Remove".length) : optionText;
+    newOption.value = optionText;
+    document.getElementById("genreList").appendChild(newOption);
+  }
 }
 
 // Event listener for when an option is selected

a7f667df504364ed29631c1dc3e4ea066ba63acb
James (personal) <jh5723@ic.ac.uk>
2023-12-06 18:10:19 +0000

    Build: Introduced song scoring system to replace sliding scale approach

--- api/app.py
+++ api/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import (
+    Flask, render_template, request,
+    redirect, url_for, session, flash
+)
 from spotipy.oauth2 import SpotifyOAuth  # type: ignore
 from spotipy import Spotify  # type: ignore
 from supabase import create_client, Client
@@ -37,114 +40,70 @@ def index():
 def get_songs_from_database(run_length_in_minutes, genres, slider_values, bool_flags):
     # Variables
     run_length_ms = run_length_in_minutes * 60000
-    # Construct query
+
+    # Construct base query
     query = (
         supabase.table("spotify_database")
-        .select(
-            "track_id",
-            "duration_ms",
-            "track_name",
-            "artists",
-            "popularity",
-            "tempo",
-            "energy",
-        )
+        .select("*")  # Select all fields to compute scores later
         .in_("track_genre", genres)
     )
 
-    # # Add conditions based on boolean flags
-    # if not bool_flags["allowExplicit"]:
-    #     query = query.eq("explicit", False)
+    # Add conditions based on boolean flags
+    if not bool_flags["includeExplicit"]:
+        query = query.eq("explicit", False)
 
-    # if bool_flags["instrumentalOnly"]:  # Adjust thresholds as appropriate
-    #     query = query.gte("instrumentalness", 0.5)
+    if bool_flags["instrumentalOnly"]:
+        query = query.gte("instrumentalness", 0.8)
 
-    # if bool_flags["includeAcoustic"]:
-    #     query = query.gte("acousticness", 0.5)
+    if bool_flags["excludeAcoustic"]:
+        query = query.lte("acousticness", 0.8)
 
-    # if bool_flags["includeLive"]:
-    #     query = query.gte("liveness", 0.5)
-    # print("Query is",query)
+    if bool_flags["excludeLive"]:
+        query = query.gte("liveness", 0.8)
+
     # Execute query
     response = query.execute()
-
     data = response.data
 
-    # Process data
-    data = response.data
-    print("Slider value:", slider_values)
+    # Print the constructed SQL query
+    print("Returned Data:", response.data)
 
-    def sorting_formula(element):
-        pop_diff = abs(slider_values["popularity"] - float(element["popularity"])) / 100
-        tempo_diff = abs(slider_values["tempo"] - float(element["tempo"])) / 140
-        energy_diff = abs(slider_values["energy"] - float(element["energy"]))
-        priority = pop_diff + tempo_diff + energy_diff
-        return priority
+    # Function to normalize values
+    def normalize(value, max_value):
+        return float(value) / max_value
 
-    data = sorted(data, key=sorting_formula)
+    # Function to compute fit score from sliding scale inputs
+    def compute_score(song):
+        score = 0
+        for attribute, (user_value, max_value, importance_weighting) in slider_values.items():
+            # Convert song's attribute value to float if it's a string
+            song_value = float(song[attribute]) if isinstance(song[attribute], str) else song[attribute]
+            song_value_normalized = normalize(song_value, max_value)
+            user_value_normalized = normalize(user_value, max_value)
+            score += (1 - abs(song_value_normalized - user_value_normalized)) * importance_weighting
+        return score # Higher score is better
 
-    selected = []
+    # Compute scores for each song
+    scored_songs = [(song, compute_score(song)) for song in data]
+
+    # Sort songs by score, descending order
+    scored_songs.sort(key=lambda x: x[1], reverse=True)
+
+    # Select songs based on run length and score
+    track_ids = []
     total_duration = 0
-
-    for song in data:
+    for song, score in scored_songs:
         if total_duration + song["duration_ms"] <= run_length_ms:
-            selected.append(song)
+            track_ids.append(song["track_id"])
             total_duration += song["duration_ms"]
         else:
-            selected.append(song)
-            total_duration += song["duration_ms"]
             break
-    return selected, total_duration
 
-
-@app.route("/fetch_songs", methods=["POST"])
-def fetch_songs():
-    run_length = float(request.form.get("run_length"))
-    genres = string_to_list(request.form.get("selectedGenres"))
-
-    # Dictionary for slider values and thresholds
-    slider_values = {
-        "energy": float(request.form.get("energyValue")),
-        "popularity": float(request.form.get("popularityValue")),
-        "danceability": float(request.form.get("danceabilityValue")),
-        "tempo": float(request.form.get("tempoValue")),
-    }
-
-    # Dictionary for boolean flags
-    bool_flags = {
-        "allowExplicit": request.form.get("allowExplicit") == "true",
-        "instrumentalOnly": request.form.get("instrumentalOnly") == "true",
-        "includeAcoustic": request.form.get("includeAcoustic") == "true",
-        "includeLive": request.form.get("includeLive") == "true",
-    }
-
-    song_data = get_songs_from_database(run_length, genres, slider_values, bool_flags)[
-        0
-    ]
-    session["track_ids"] = [i["track_id"] for i in song_data]
-    session["titles"] = [i["track_name"] for i in song_data]
-    session["artists"] = [i["artists"].replace(";", ", ") for i in song_data]
-
-    if not genres[0]:  # No genre selected
-        flash("Please select at least one genre")
-        return redirect(url_for("index", _external=True))
-    else:
-        return redirect(url_for("export", _external=True))
-
-
-@app.route("/export")
-def export():
-    return render_template("export.html")
-
+    return track_ids, total_duration
 
 # Request Spotify to log the user in
 @app.route("/generate", methods=["POST", "GET"])
 def generate():
-    name = bleach.clean(request.form.get("name"))
-    description = bleach.clean(request.form.get("description"))
-    session["new_playlist_headers"] = {}
-    session["new_playlist_headers"]["name"] = name
-    session["new_playlist_headers"]["description"] = description
     session.permanent = True
     sp_oauth = get_spotify_oauth()
     # The authorise_url is where Spotify redirects you once
@@ -181,16 +140,15 @@ def create_playlist():
     try:
         sp.user_playlist_create(
             user_id,
-            name=session["new_playlist_headers"]["name"],
-            description=session["new_playlist_headers"]["description"],
+            name=session['new_playlist_headers']['name'],
+            description=session['new_playlist_headers']['description']
         )
     except Exception as e:
         return "Error creating playlist: " + str(e)
     # Check that this is the right playlist
     last_playlist_id = sp.user_playlists(user_id)["items"][0]["id"]
-    session["playlist_url"] = "https://open.spotify.com/playlist/" + last_playlist_id
     # ADD ITEMS TO PLAYLIST
-    ids_to_add = session.get("track_ids")
+    ids_to_add = session.get("track_ids")[0]
     try:
         sp.playlist_add_items(last_playlist_id, ids_to_add)
     except Exception:
@@ -211,7 +169,46 @@ def get_spotify_oauth():
 # to your playlist instead?
 @app.route("/success", methods=["GET"])
 def success():
-    return render_template("success.html")
+    return render_template("success_page.html")
+
+
+@app.route("/fetch_songs", methods=["POST"])
+def fetch_songs():
+    run_length = float(request.form.get("run_length"))
+    genres = string_to_list(request.form.get("selectedGenres"))
+
+    # Print data received from form
+    print("Received form data:", request.form)
+
+    # Dictionary for slider values and corresponding max values
+    slider_values = {   # (input_value, max_possible_value, importance_weighting)
+        "energy": (float(request.form.get("energyValue")), 1, 1),
+        "popularity": (float(request.form.get("popularityValue")), 100, 1),
+        "danceability": (float(request.form.get("danceabilityValue")), 1, 1),
+        "tempo": (float(request.form.get("tempoValue")), 220, 1)
+    }
+
+    # Dictionary for boolean flags
+    bool_flags = {
+        "includeExplicit": request.form.get("includeExplicit") == 'true',
+        "instrumentalOnly": request.form.get("instrumentalOnly") == 'true',
+        "excludeAcoustic": request.form.get("excludeAcoustic") == 'true',
+        "excludeLive": request.form.get("excludeLive") == 'true'
+    }
+
+    session['new_playlist_headers'] = {
+        'name': bleach.clean(request.form.get("name", "")),
+        'description': bleach.clean(request.form.get("description", ""))
+    }
+
+    track_ids, total_duration = get_songs_from_database(run_length, genres, slider_values, bool_flags)
+    session["track_ids"] = track_ids
+
+    if not genres[0]:  # No genre selected
+        flash("Please select at least one genre")
+        return redirect(url_for('index', _external=True))
+    else:
+        return redirect(url_for("success", _external=True))
 
 
 def string_to_list(input_string):
